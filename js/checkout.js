93ff84ac22cf563fdbabbd3a559f7a1ecb687d82
Mr Ripper <736234860@qq.com>
2017-06-03 22:43:26 +0800

    最后

--- js/checkout.js
+++ js/checkout.js
@@ -15,6 +15,8 @@
     oTime2.onblur = function(){
         time2 = this.value;
     };
+    time1 = time1.replace("T",' ');
+    time2 = time2.replace("T",' ');
     // oChoice1.onblur = function(){
     //     choice1 = this.value;
     // };
@@ -22,89 +24,132 @@
     console.log(time2);
     oBtn.onclick = function(){
         // <创建XMLrequest对象>
-
-        if(window.XMLHttpRequest){
-            var xmlhttp = new XMLHttpRequest();
-        }else{
-            var xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
-        }
-        // time1.replace(/T/g,' ');
-        // time2.replace(/T/g,' ');
-        time1 = time1.replace("T",' ');
-        time2 = time2.replace("T",' ');
-
-
-
-
-        //发送请求
-        xmlhttp.open('POST','js/servers.php',true);
-        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-        xmlhttp.send('first='+time1+'&last='+time2);//'time1='+time1+'&time2='+time2
-        xmlhttp.onreadystatechange = function() {
-
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                console.log(xmlhttp.responseText);
-                var obj = JSON.parse(xmlhttp.responseText);
-                // console.log(obj);
-
-                oDiv.innerHTML = xmlhttp.responseText;
-
-                // 基于准备好的dom，初始化echarts实例
-                var myChart = echarts.init(oDiv);
-
-                 // 指定图表的配置项和数据
-                 var base = +new Date(1901, 2, 0);
-                 var oneDay = 24 * 3600 * 1000;
-                 //var date = [];
-
-
-
-                 for (var i = 1; i < 365; i++) {
-                 var now = new Date(base += oneDay);
-                 //date.push([now.getFullYear(), now.getMonth() + 1, now.getDate(),now.getHours()].join('/'));
-
-                 }
-
-                option = {
-                    title : {
-                        text: '1901年海洋温度监测数据来源',
-                        subtext: '数值符合',
-                        x:'center'
-                    },
-                    tooltip : {
-                        trigger: 'item',
-                        formatter: "{a} <br/>{b} : {c} ({d}%)"
-                    },
-                    legend: {
-                        orient: 'vertical',
-                        left: 'left',
-                        data: ['极度寒冷','寒冷','危险','冰凉','舒适']
-                    },
-                    series : [
-                        {
-                            name: '访问来源',
-                            type: 'pie',
-                            radius : '55%',
-                            center: ['50%', '60%'],
-                            data:[
-                                {value:obj.num[0], name:'极度寒冷'},
-                                {value:obj.num[1], name:'寒冷'},
-                                {value:obj.num[2], name:'危险'},
-                                {value:obj.num[3], name:'冰凉'},
-                                {value:obj.num[4], name:'舒适'}
-                            ],
-                            itemStyle: {
-                                emphasis: {
-                                    shadowBlur: 10,
-                                    shadowOffsetX: 0,
-                                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                                }
-                            }
-                        }
-                    ]
-                };
-                 // 使用刚指定的配置项和数据显示图表。
-                 myChart.setOption(option);
+        setInterval(function(){
+            if(window.XMLHttpRequest){
+                var xmlhttp = new XMLHttpRequest();
+            }else{
+                var xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
             }
-        }
+            // time1.replace(/T/g,' ');
+            // time2.replace(/T/g,' ');
+
+
+
+
+
+            //发送请求
+            xmlhttp.open('POST','js/servers.php',true);
+            xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            xmlhttp.send('first='+time1+'&last='+time2);//'time1='+time1+'&time2='+time2
+            xmlhttp.onreadystatechange = function() {
+
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    console.log(xmlhttp.responseText);
+                    var obj = JSON.parse(xmlhttp.responseText);
+                    console.log(obj);
+                    oDiv.innerHTML = "";
+                    oDiv.innerHTML = xmlhttp.responseText;
+
+                    // 基于准备好的dom，初始化echarts实例
+                    var myChart = echarts.init(oDiv);
+
+                    // 指定图表的配置项和数据
+
+                    var date = [];
+
+                    var now = new Date(base += oneDay);
+
+
+                    var base = +new Date(1901, 2, 0);
+                    var oneDay = 24 * 3600 * 1000;
+                    //var date = [];
+
+
+
+                    for (var i = 1; i < 365; i++) {
+                        date.push([now.getFullYear(), now.getMonth(), now.getDate()].join('/'));
+                        //date.push([now.getFullYear(), now.getMonth() + 1, now.getDate(),now.getHours()].join('/'));
+
+                    }
+
+                    option = {
+                        tooltip: {
+                            trigger: 'axis',
+                            position: function (pt) {
+                                return [pt[0], '10%'];
+                            }
+                        },
+                        title: {
+                            left: 'center',
+                            text: '大数据量面积图',
+                        },
+                        toolbox: {
+                            feature: {
+                                dataZoom: {
+                                    yAxisIndex: 'none'
+                                },
+                                restore: {},
+                                saveAsImage: {}
+                            }
+                        },
+                        xAxis: {
+                            type: 'category',
+                            boundaryGap: false,
+                            data: obj.arrt
+                            // [<?php echo $str;?>]
+                        },
+                        yAxis: {
+                            type: 'value',
+                            boundaryGap: [0, '100%']
+                        },
+                        dataZoom: [{
+                            type: 'inside',
+                            start: 0,
+                            end: 10
+                        }, {
+                            start: 0,
+                            handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+                            handleSize: '80%',
+                            handleStyle: {
+                                color: '#fff',
+                                shadowBlur: 3,
+                                shadowColor: 'rgba(0, 0, 0, 0.6)',
+                                shadowOffsetX: 2,
+                                shadowOffsetY: 2
+                            }
+                        }],
+                        series: [
+                            {
+                                name:'温度',
+                                type:'line',
+                                smooth:true,
+                                symbol: 'none',
+                                sampling: 'average',
+                                itemStyle: {
+                                    normal: {
+                                        color: 'rgb(255, 70, 131)'
+                                    }
+                                },
+                                areaStyle: {
+                                    normal: {
+                                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                            offset: 0,
+                                            color: 'rgb(255, 158, 68)'
+                                        }, {
+                                            offset: 1,
+                                            color: 'rgb(255, 70, 131)'
+                                        }])
+                                    }
+                                },
+                                data: date
+                                // markPoint:{data:[{type:'max',name:'最大值'},{type:'min',name:'最小值'}]}
+                            }
+                        ]
+                    };
+                    // 使用刚指定的配置项和数据显示图表。
+                    myChart.setOption(option);
+                }
+            }
+        },1000);
+
     };
--- js/servers.php
+++ js/servers.php
@@ -10,7 +10,7 @@
 //    echo $time1.$time2;
 
 
-    $conn = mysqli_connect('localhost','root','','blog');
+    $conn = mysqli_connect('localhost','root','','xuhui');
     mysqli_set_charset($conn,'utf8');//上面两句必须同时使用
     if (!$conn){
         die("Connection failed: " . mysqli_connect_error());
@@ -24,68 +24,36 @@
 //    }else if($choice == 2){
 //        $result = mysqli_query($conn,"SELECT * FROM test WHERE year_month_day>"."'$first'"."and year_month_day<"."'$last'".'order by temperature DESC');
 //    }
-    $result = mysqli_query($conn,"SELECT * FROM test WHERE year_month_day>"."'$first'"."and year_month_day<"."'$last'".'order by year_month_day ASC');
+    $result = mysqli_query($conn,"SELECT * FROM xuhui");
 //    $number = 0;
 
-    $arr1 = array();
-//    $arr1->num = 0;
-    $arr2 = array();
-//    $arr2->num = 0;
-    $arr3 = array();
-//    $arr3->num = 0;
-    $arr4 = array();
-//    $arr4->num = 0;
-    $arr5 = array();
-//    $arr5->num = 0;
-    $num = array(0,0,0,0,0);
 
-    $str = array();
+    $arrt = array();
+    $arrh = array();
+    $arrs = array();
+    $arra = array();
 
-    define("a",-100);
-    define("b",-40);
-    define("c",-10);
-    define("d",20);
+
+
     while($row = mysqli_fetch_array($result))//函数从结果集中取得一行作为关联数组
     {
-        if($row["temperature"]<a){
-            $arr1[] = $row["temperature"];
-//            $arr1->num = $arr1->num + 1;
-            $num[0]++;
-        }elseif ($row["temperature"]<b && $row["temperature"]>=a){
-            $arr2[] = $row["temperature"];
-//            $arr2->num = $arr2->num + 1;
-            $num[1]++;
-        }elseif ($row["temperature"]<c && $row["temperature"]>=b){
-            $arr3[] = $row["temperature"];
-//            $arr3->num = $arr3->num + 1;
-            $num[2]++;
-        }elseif ($row["temperature"]<d && $row["temperature"]>=c){
-            $arr4[] = $row["temperature"];
-//            $arr4->num = $arr4->num + 1;
-            $num[3]++;
-        }elseif($row["temperature"]>d){
-            $arr5[] = $row["temperature"];
-//            $arr5->num = $arr5->num + 1;
-            $num[4]++;
-        }
 //        $number++;
 //        echo '<tr>'.
 //                '<td>'.$number.'</td>'.
 //                '<td>'.$row["year_month_day"].'</td>'.
 //                '<td>'.$row["temperature"].'</td>'.
 //              '</tr>';
-        $str[] = $row['year_month_day'];
-
+        $arrt[] = $row['temput'];
+        $arrh[] = $row['humi'];
+        $arrs[] = $row['sun'];
+        $arra[] = $row['alcohol'];
     }
 
     $ob = array(
-        'str'=>$str,
-        'arr1'=>$arr1,
-        'arr2'=>$arr2,
-        'arr3'=>$arr3,
-        'arr4'=>$arr4,
-        'arr5'=>$arr5,
-        'num'=>$num
+        'arrt'=>$arrt,
+        'arrh'=>$arrh,
+        'arrs'=>$arrs,
+        'arra'=>$arra
     );
     echo json_encode($ob);
 ?>
